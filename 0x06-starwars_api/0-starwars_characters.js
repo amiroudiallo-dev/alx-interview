52b13d21d7e8b9569c3f781b22746e4a8ee7703d
Amirou  DIALLO <amiroudiallo.yw@gmail.com>
2024-08-09 09:33:00 +0000

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,39 @@
 #!/usr/local/bin/node
 const request = require('request');
-const API_URL = 'https://swapi-api.alx-tools.com/api/';
+const API_URL = 'https://swapi-api.alx-tools.com/api';
 
 if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+  const filmId = process.argv[2];
+  request(`${API_URL}/films/${filmId}/`, (err, response, body) => {
     if (err) {
-      console.log(err);
+      console.log('Error:', err);
+      return;
     }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          resolve(JSON.parse(charactersReqBody).name);
-        });
-      }));
 
-    Promise.all(charactersName)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
+    if (response.statusCode !== 200) {
+      console.log(`Failed to fetch data. Status code: ${response.statusCode}`);
+      return;
+    }
+
+    try {
+      const filmData = JSON.parse(body);
+      const charactersURL = filmData.characters;
+      const charactersName = charactersURL.map(
+        url => new Promise((resolve, reject) => {
+          request(url, (promiseErr, _, charactersReqBody) => {
+            if (promiseErr) {
+              reject(promiseErr);
+            }
+            resolve(JSON.parse(charactersReqBody).name);
+          });
+        })
+      );
+
+      Promise.all(charactersName)
+        .then(names => console.log(names.join('\n')))
+        .catch(allErr => console.log(allErr));
+    } catch (parseError) {
+      console.log('Failed to parse JSON:', parseError.message);
+    }
   });
 }
